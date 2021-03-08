191fc548f3ead4467517c14813e3e72d10cf2a44
alohabeach <46428270+alohabeach@users.noreply.github.com>
2021-03-07 22:26:24 -1000

    Update JailbreakHashes.lua

--- Scripts/JailbreakHashes.lua
+++ Scripts/JailbreakHashes.lua
@@ -1,1 +1,1 @@
-{"Damage":"f1ba3b54","EquipServer":"b0d32de1","PopTire":"ffebe8b9","RopeAttach":"ff152e6f"}
+{"Damage":"aa4357d8","EquipServer":"f2fc051e","PopTire":"b2bafd0e","RopeAttach":"cc8d09ac"}
