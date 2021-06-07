31835a72489881730b03eec065576cac5f6c8c55
alohabeach <46428270+alohabeach@users.noreply.github.com>
2021-06-06 15:27:06 -1000

    Create Main.lua

---
+++ Scripts/Main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 606849621 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/alohabeach/Main/master/Scripts/JailbreakAutoRob.lua"))()
+elseif game.PlaceId == 185655149 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/alohabeach/Main/master/Scripts/BloxburgAutoFarm"))()
+end
