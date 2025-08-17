74c6d604b389a9f99a5d26765b79a0727783629f
alohabeach <jackrahunziker@gmail.com>
2025-08-17 10:22:49 -1000

    Revert "Temporary test removal for seliware"
    This reverts commit 5a638a3648470a05bd4cf65d7a232b76bc3faec2.

--- utils/unc-check/source.lua
+++ utils/unc-check/source.lua
@@ -779,11 +779,11 @@ addTest("queue_on_teleport", {"queueonteleport"}, function()
 	local queue_on_teleport = queue_on_teleport or queueonteleport
 	assert(type(queue_on_teleport) == "function", "Neither 'queue_on_teleport' nor 'queueonteleport' function is available in the current environment")
 
-	-- local success, err1 = pcall(function() queue_on_teleport("") end)
-	-- local success2 = pcall(function() queue_on_teleport() end)
+	local success, err1 = pcall(function() queue_on_teleport("") end)
+	local success2 = pcall(function() queue_on_teleport() end)
 	
-	-- assert(success, string.format("Function should accept empty string but threw error: %s", err1 or "unknown error"))
-	-- assert(not success2, string.format("Function should reject nil/missing arguments but succeeded unexpectedly (expected error but got none)"))
+	assert(success, string.format("Function should accept empty string but threw error: %s", err1 or "unknown error"))
+	assert(not success2, string.format("Function should reject nil/missing arguments but succeeded unexpectedly (expected error but got none)"))
 end)
 
 addTest("request", {"http.request", "http_request"}, function()
